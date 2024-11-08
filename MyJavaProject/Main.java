cb2ba9aa4ca797bb1b758c682a06696f03f22331
Gizachew Belayneh <49734635+Gize2011@users.noreply.github.com>
2024-11-08 15:00:29 +0300

    Update Main.java

--- MyJavaProject/Main.java
+++ MyJavaProject/Main.java
@@ -1,1 +1,15 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to My Java Project!");
+        
+        // Example method call
+        greetUser("John");
+    }
+
+    // A simple method to greet the user
+    public static void greetUser(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
 
